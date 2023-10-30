36f751756c5159737d318c5a57a4390a41b58577
brk-a <fnjakai@outlook.com>
2023-10-30 15:03:24 +0300

    set up mongo db

--- 0x04-learn_node/fn-api/src/app.module.ts
+++ 0x04-learn_node/fn-api/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
+
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.vmxrkgp.mongodb.net/?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
