1f97c7fc1f930d80e62431e4d9e29882ad7761ed
brk-a <fnjakai@outlook.com>
2023-11-04 21:55:16 +0300

    create auth dto

--- 0x01-learn_node/fn-bookmarks/src/auth/auth.service.ts
+++ 0x01-learn_node/fn-bookmarks/src/auth/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable} from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2"
 
-@Injectable({})
+@Injectable()
 export class AuthService{
-    login(){
-        return "Logged in"
+
+    constructor(private prisma: PrismaService){}
+    async login(dto: AuthDto){
+        const hash = await argon.hash(dto.password)
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: hash,
+                first_name: dto.firstName,
+                last_name: dto.lastName,
+            },
+        })
+        delete user.password // do not return passwd hash
+        return user
     }
 
     signup(){
