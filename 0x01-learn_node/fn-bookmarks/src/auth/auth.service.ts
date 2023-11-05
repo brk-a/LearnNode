4162d0ad4748f5d36e3f6e4290d00098936a8bf1
brk-a <fnjakai@outlook.com>
2023-11-05 05:55:20 +0300

    ..

--- 0x01-learn_node/fn-bookmarks/src/auth/auth.service.ts
+++ 0x01-learn_node/fn-bookmarks/src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable} from "@nestjs/common";
+import { ForbiddenException, Injectable} from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService{
@@ -9,16 +10,26 @@ export class AuthService{
     constructor(private prisma: PrismaService){}
     async login(dto: AuthDto){
         const hash = await argon.hash(dto.password)
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                password: hash,
-                first_name: dto.firstName,
-                last_name: dto.lastName,
-            },
-        })
-        delete user.password // do not return passwd hash
-        return user
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    password: hash,
+                    first_name: dto.firstName,
+                    last_name: dto.lastName,
+                },
+            })
+            delete user.password // do not return passwd hash
+            return user
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code==='P2002'){
+                    throw new ForbiddenException("credentials taken")
+                }
+            }
+            console.info("authServiceError", error)
+            throw error
+        }
     }
 
     signup(){
